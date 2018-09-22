b75ad38134c403e1ed16df8cf4260bd17ead04cf
Kahoul <646676256@qq.com>
2018-09-23 01:51:43 +0800

    project initialized2

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div >
+     <div>this is a Home page</div>
+     <router-link to="/list">跳转到list列表项</router-link>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
