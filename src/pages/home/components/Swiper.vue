d75509b242599c6ec9908079ac7e9e22968b5a1c
Kahoul <646676256@qq.com>
2018-09-24 18:18:18 +0800

    add swiper besides wrap

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,32 +1,54 @@
 <template>
-  <swiper :options="swiperOption">
-    <!-- slides -->
-    <swiper-slide>I'm Slide 1</swiper-slide>
-    <swiper-slide>I'm Slide 2</swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
-    <swiper-slide>I'm Slide 4</swiper-slide>
-    <swiper-slide>I'm Slide 5</swiper-slide>
-    <swiper-slide>I'm Slide 6</swiper-slide>
-    <swiper-slide>I'm Slide 7</swiper-slide>
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-    <div class="swiper-button-prev" slot="button-prev"></div>
-    <div class="swiper-button-next" slot="button-next"></div>
-    <div class="swiper-scrollbar"   slot="scrollbar"></div>
-  </swiper>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for='item of swiperList' :key='item.id'>
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+      <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+    </swiper>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomeSwiper',
-  data: function(){
+  data: function () {
   	return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1809/50/670c61165170f902.jpg_750x200_c4b8d1dc.jpg" 
+        },
+        {
+          id: '002',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1806/1c/4847ea66072c7b02.jpg_750x200_c32457fb.jpg" 
+        }
+      ]
   	}
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  .wrapper{
+    width: 7.5rem;
+    height: 2.04rem;
+    background-color: #eee;
+  }
 
+  .swiper-img{
+    width: 100%;
+    height: 100%;
+  }
 </style>
