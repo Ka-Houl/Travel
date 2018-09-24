4c83fac8805ca485fbc69390eca909f21825f319
Kahoul <646676256@qq.com>
2018-09-24 21:36:21 +0800

    add swipperand wrap

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -9,9 +9,9 @@
 
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
-      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
-      <div class="swiper-button-next" slot="button-next"></div>
-      <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+<!--       <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div> -->
+      <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
     </swiper>
   </div>
 </template>
@@ -22,9 +22,13 @@ export default {
   data: function () {
   	return {
       swiperOption: {
-        pagination: '.swiper-pagination',
-        loop: true
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        loop: true,
+        keyboard : true,
       },
+
       swiperList: [
         {
           id: '001',
@@ -33,14 +37,22 @@ export default {
         {
           id: '002',
           imgUrl: "http://img1.qunarzz.com/piao/fusion/1806/1c/4847ea66072c7b02.jpg_750x200_c32457fb.jpg" 
-        }
-      ]
+        },
+        {
+          id: '003',
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1808/70/5603e6a535062402.jpg_750x200_2c552f2c.jpg" 
+        }        
+      ],
+
   	}
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active{
+    background-color: #fff 
+  }
   .wrapper{
     width: 7.5rem;
     height: 2.04rem;
