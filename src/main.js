c4d1c1f3e615dad7b60dfb599ac2fd1c74ca585e
Kahoul <646676256@qq.com>
2018-09-29 12:09:11 +0800

    city list add scroll

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
+import VueAwesomeSwiper from 'vue-awesome-swiper'   //引入轮播图
 import FastClick from 'fastclick'
 import App from './App'
 import router from './router'
